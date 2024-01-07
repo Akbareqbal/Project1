34c24bb07c71fb75186f22babf1fe9600b18575a
akbar <akbar@Akbar.lan>
2024-01-07 16:19:11 -0600

    Initial Commit

---
+++ src/Demo/TestDemo.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class TestDemo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("GitHub Stated....");
+		
+		System.out.println("QA Team3...");
+		
+		System.out.println("Java Project");
+	}
+
+}
